05715ff2290f90481d07b92e43ed60c63696b49e
Taimo <taimo1999.5@gmail.com>
2019-02-19 21:08:57 +0900

    sign up improvement2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to root_url
+      redirect_to '/'
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
